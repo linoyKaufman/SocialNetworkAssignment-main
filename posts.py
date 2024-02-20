46507e8d04c86dbdea8325d350b1923de7948713
mor <morberger444@gmail.com>
2024-02-20 13:18:03 +0200

    work

--- posts.py
+++ posts.py
@@ -1,7 +1,34 @@
-class Posts:
-    def __init__ (self, like, comment,type):
-            self.like = []
-            self.comment = []
+class posts:
+    def __init__ (self,user,type,info,price,place):
+            self.likes = []
+            self.comments = []
             self.type = type
+            self.info=info
+            self.availble=True
+            self.price=price
+            self.place=place
+            self.user=user
+
+    def like(self,user1):
+            if not any(user1 == self.likes for users in self.likes):
+                    self.likes.append(user1)
+
+    def unlike(self, user1):
+        if any(user1 == self.likes for users in self.likes):
+            self.likes.remove(user1)
+
+    def comment(self, user1, comm):
+            self.comments.append((user1, comm))
+
+    def discount(self,percent,password):
+        if self.type=="Sale" and password==self.user.password:
+            self.price= self.price * (1 - 0.01 * percent)
 
 
+    def sold(self,password):
+        if self.type=="Sale" and password==self.user.password:
+            self.availble=False
+
+    def display (self):
+        if self.type == "Image":
+            self.availble = False
--- users.py
+++ users.py
@@ -1,22 +1,39 @@
-#from posts import posts
+from posts import posts
+
 
 class users:
-    def __init__ (self, name, password):
+    def __init__(self, name, password):
         self.name = name
         self.password = password
-        self.status=False
-        self.following= []
-        self.posts = []
+        self.status = False
+        self.following = []
+        self.post = []
+        self.notifications=[]
 
+    def set_status(self):
+        self.status = not self.status
 
-    def set_status (self):
-        self.status= not self.status
+    def follow(self, user1):
+        if not any(user1 == self.following for users in self.following):
+            self.following.append(user1)
 
-    def follow(self, user):
-        if not any (user==self.following for user in self.following):
-            self.following.append(user)
-    
-    def unfollow(self, user):
-        if any(user==self.following for user in self.following):
-            self.following.remove(user)
+    def unfollow(self, user1):
+        if any(user1 == self.following for users in self.following):
+            self.following.remove(user1)
 
+    def publish_post(self, type, info,price=None, place=None):
+        if not any(self.post.info == info for post in self.post):
+            if type == "Text" or type == "Image":
+                post1 = posts(self,type, info,price,place)
+                self.post.append(post1)
+            if type == "Sale":
+                post1 = posts(self,type, info, price, place)
+                self.post.append(post1)
+            return post1
+
+    def print_notifications(self):
+        for notification in self.notifications:
+            print(notification)
+
+    def add_notifications(self,notif):
+        self.notifications.append(notif)
